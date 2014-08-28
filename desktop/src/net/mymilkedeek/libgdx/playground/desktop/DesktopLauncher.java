8adfc06d8048b2f3df9ac056a2e521e6f83386df
MyMilkedEek <ghb+4rqw>
2014-08-28 07:39:34 +0200

    initial commit

---
+++ desktop/src/net/mymilkedeek/libgdx/playground/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mymilkedeek.libgdx.playground.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mymilkedeek.libgdx.playground.GameInstance;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameInstance(), config);
+	}
+}
---
+++ html/src/net/mymilkedeek/libgdx/playground/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.mymilkedeek.libgdx.playground.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.mymilkedeek.libgdx.playground.GameInstance;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GameInstance();
+        }
+}
