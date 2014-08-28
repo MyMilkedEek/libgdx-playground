89295630665841108b76ff91fa2a5cc994724e88
MyMilkedEek <ghb+4rqw>
2014-08-28 07:44:25 +0200

    gameinstance artemis

--- core/src/net/mymilkedeek/libgdx/playground/GameInstance.java
+++ core/src/net/mymilkedeek/libgdx/playground/GameInstance.java
@@ -1,27 +1,87 @@
 package net.mymilkedeek.libgdx.playground;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.artemis.Entity;
+import com.artemis.World;
+import com.artemis.systems.event.BasicEventDeliverySystem;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.input.GestureDetector;
+import net.mymilkedeek.random.components.PositionComponent;
+import net.mymilkedeek.random.events.systems.MoveCameraSystem;
+import net.mymilkedeek.random.systems.RenderingSystem;
+import net.mymilkedeek.random.systems.ResetPositionSystem;
+import net.mymilkedeek.random.systems.input.InputSystem;
 
-public class GameInstance extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class GameInstance extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    public static final int GAME_WIDTH = 400;
+    public static final int GAME_HEIGHT = 400;
+
+    private World world;
+    private OrthographicCamera camera;
+
+    @Override
+    public void create() {
+        // create a camera
+        camera = new OrthographicCamera(GAME_WIDTH, GAME_HEIGHT);
+        createWorld();
+        // create a few of entities
+        createEntity(200, 250, 80, 80);
+        createEntity(200, 200, -80, 80);
+        createEntity(200, 150, 80, -80);
+        createEntity(200, 100, -80, -80);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        world.setDelta(Gdx.graphics.getDeltaTime());
+        world.process();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        float centerX = width / 2.0f;
+        float centerY = height / 2.0f;
+        this.camera.position.set(centerX, centerY, 0);
+        this.camera.viewportWidth = width;
+        this.camera.viewportHeight = height;
+    }
+
+    /**
+     * Creates the world and adds entity systems to it.
+     */
+    private void createWorld() {
+        world = new World();
+        world.setEventDeliverySystem(new BasicEventDeliverySystem());
+        //world.setSystem(new MovementSystem());
+        world.setSystem(new ResetPositionSystem());
+        world.setSystem(new RenderingSystem(camera, Color.WHITE));
+
+        InputSystem inputSystem = new InputSystem();
+        InputMultiplexer inputMultiplexer = new InputMultiplexer();
+        inputMultiplexer.addProcessor(inputSystem);
+        inputMultiplexer.addProcessor(new GestureDetector(inputSystem));
+        Gdx.input.setInputProcessor(inputMultiplexer);
+        world.setSystem(inputSystem);
+        world.setSystem(new MoveCameraSystem(camera));
+
+        world.initialize();
+    }
+
+    private void createEntity(float x, float y, float vx, float vy) {
+        Entity e = world.createEntity();
+        PositionComponent pc = world.createComponent(PositionComponent.class);
+        pc.position.set(x, y);
+        e.addComponent(pc);
+        /*VelocityComponent vc = world.createComponent(VelocityComponent.class);
+        vc.velocity.set(vx, vy);
+        e.addComponent(vc);*/
+        e.addToWorld();
+    }
 }
